39d31e39d23e757aad999df27a5ee38a1a3bded0
bluedinos <yasminwassie@gmail.com>
2024-11-28 00:11:14 +0100

    Initial commit: Set up Tkinter window

---
+++ mainwindow.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# creating main application window
+window=tk.Tk()
+window.title("Fractal Matching Game") 
+window.geometry("600x800") #set to potrait orientation
+window.resizable(False,False) # making window non resizable
+
+# Run the Tkinter event loop
+window.mainloop()
