4f341dc00b054b51ea0c480e0ebb5e0bbc2c64c4
bluedinos <yasminwassie@gmail.com>
2024-12-01 01:01:27 +0100

    Initial GUI setup with canvas and sliders
    - Created main application window with portrait orientation.
    - Added canvas for fractal display.
    - Implemented six adjustable sliders for fractal parameters.
    - Designed grid layout for sliders with proper alignment.
    
    Worked on by: Yasmin I Wassie and Pavlo Asieiev

--- mainwindow.py
+++ mainwindow.py
@@ -1,10 +1,39 @@
 import tkinter as tk
 
-# creating main application window
+''' creating main application window'''
 window=tk.Tk()
 window.title("Fractal Matching Game") 
-window.geometry("600x800") #set to potrait orientation
+window.geometry("600x750") #set to potrait orientation
 window.resizable(False,False) # making window non resizable
 
-# Run the Tkinter event loop
+'''Adds a canvas for the fractal display '''
+canvas = tk.Canvas(window, bg="white", width=500, height=450)
+canvas.grid(row=0, column=0, columnspan=3, pady=(20, 10), padx=(50, 0))
+
+'''Add a frame for the sliders below the canvas'''
+slidersLabels = tk.Frame(window)
+slidersLabels.grid(row=1, column=0, columnspan=3, pady=10)
+
+'''Add sliders for 6 different parameters'''
+off_scaleScale = tk.Scale(slidersLabels, label="off_scale", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+off_scaleScale.grid(row=0, column=0, padx=10, pady=10)
+
+angle_detaScale = tk.Scale(slidersLabels, label="angle_deta", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+angle_detaScale.grid(row=0, column=1, padx=10, pady=10)
+
+n_splitsScale = tk.Scale(slidersLabels, label="n_splits", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+n_splitsScale.grid(row=1, column=0, padx=10, pady=10)
+
+length_ratioScale = tk.Scale(slidersLabels, label="length_ratio", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+length_ratioScale.grid(row=1, column=1, padx=10, pady=10)
+
+init_lengthScale = tk.Scale(slidersLabels, label="init_length", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+init_lengthScale.grid(row=2, column=0, padx=10, pady=10)
+
+width_ratioScale = tk.Scale(slidersLabels, label="width_ratio", from_=0, to=100, orient=tk.HORIZONTAL, length=120)
+width_ratioScale.grid(row=2, column=1, padx=10, pady=10)
+
+'''Run the Tkinter event loop'''
 window.mainloop()
+
+
