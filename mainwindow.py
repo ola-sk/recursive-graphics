820cc6920e20728a3c9a7abc9a431b01f36b798f
ola-sk <50403262+ola-sk@users.noreply.github.com>
2024-12-10 05:25:57 +0100

    Add fractal tree drawing functionality to GUI canvas
    Implements ability to draw recursive fractal trees in the canvas view:
    - Adds new draw_fractal_tree function with breadth-first traversal
    - Updates main window to use fractal tree instead of squares
    - Configures initial tree parameters for demo visualization
    
    Improves code organization by extracting canvas dimensions into variables

--- mainwindow.py
+++ mainwindow.py
@@ -6,11 +6,13 @@ adds sliders for various parameters, and binds events for canvas zooming.
 """
 
 import tkinter as tk
-
+from numpy import radians
 from gui import (initialise_gui,
                  populate_sliders,
                  bind_canvas_zoom_events)
-from sandbox import draw_squares
+from sandbox import draw_squares, draw_fractal_tree
+from datastructures import TreeNodeBase
+
 
 # Main application window
 window_width = 1300
@@ -19,10 +21,12 @@ window = initialise_gui(window_width, window_height)
 
 # place Canvas in the window
 canvas_padding_x = 0
+canvas_width = window_width - 2 * canvas_padding_x
+canvas_height = 535
 canvas = tk.Canvas(
     window, bg="white",
-    width=window_width - 2 * canvas_padding_x,
-    height=535
+    width=canvas_width,
+    height=canvas_height
 )
 canvas.grid(
     row=0, column=0, columnspan=2,
@@ -36,7 +40,12 @@ sliders_frame.grid(row=1, column=0, columnspan=3, pady=10)
 # Create sliders
 populate_sliders(sliders_frame, 4)
 
-draw_squares(canvas)
+draw_fractal_tree(
+    canvas,
+    TreeNodeBase(
+        canvas_width/2, canvas_height, 150, radians(-90), 3, 0.7, radians(25), max_depth=7
+    )
+)
 bind_canvas_zoom_events(canvas)
 
 # Run the Tkinter event loop
--- sandbox/draw_on_canvas.py
+++ sandbox/draw_on_canvas.py
@@ -17,6 +17,7 @@ Usage example:
     window.mainloop()
 """
 import tkinter as tk
+from datastructures import TreeNodeBase, traverse_breadth_first
 
 
 def draw_squares(canvas: tk.Canvas, num_squares: int = 8, square_size: float = 50, color: str = "blue") -> None:
@@ -35,3 +36,21 @@ def draw_squares(canvas: tk.Canvas, num_squares: int = 8, square_size: float = 5
         x = i * square_size
         y = i * square_size
         canvas.create_rectangle(x, y, x + square_size, y + square_size, outline=color, fill=color)
+
+
+def draw_fractal_tree(canvas: tk.Canvas, root_node: TreeNodeBase=None) -> None:
+    """
+    Draw a fractal tree on the canvas.
+    Args:
+        canvas: tkinter.Canvas object
+        root_node: Node object representing the fractal tree (root node)
+
+    Returns:
+        None
+    """
+    def draw_line(node: TreeNodeBase):
+        canvas.create_line(node.start_x, node.start_y, node.end_x, node.end_y, fill="green")
+    if root_node is None:
+        return
+    traverse_breadth_first(root_node, draw_line)
+
